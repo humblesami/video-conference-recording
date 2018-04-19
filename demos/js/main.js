f28aa5d79b0b1a1c244629c8f81176fa73471833
samiciit <sami.akram@digitalnet.com>
2018-04-19 14:23:16 +0500

    init

---
+++ demos/js/main.js
@@ -0,0 +1,184 @@
+// ......................................................
+            // .......................UI Code........................
+            // ......................................................
+            
+            document.getElementById('share-screen').onclick = function() {
+                
+                connection.addStream({
+                    screen: true,
+                    oneway: true
+                });
+            };
+
+            document.getElementById('open-room').onclick = function() {
+                disableInputButtons();
+                connection.open(document.getElementById('room-id').value, function() {
+                    showRoomURL(connection.sessionid);
+                });
+            };
+
+            document.getElementById('join-room').onclick = function() {
+                disableInputButtons();
+                connection.join(document.getElementById('room-id').value);
+            };
+
+            document.getElementById('open-or-join-room').onclick = function() {
+                disableInputButtons();
+                connection.openOrJoin(document.getElementById('room-id').value, function(isRoomExists, roomid) {
+                    if(!isRoomExists) {
+                        showRoomURL(roomid);
+                    }
+                });
+            };
+
+            // ......................................................
+            // ..................RTCMultiConnection Code.............
+            // ......................................................
+
+            var connection = new RTCMultiConnection();
+            
+            // Using getScreenId.js to capture screen from any domain
+            // You do NOT need to deploy Chrome Extension YOUR-Self!!
+            connection.getScreenConstraints = function(callback) {
+                getScreenConstraints(function(error, screen_constraints) {
+                    if (!error) {
+                        screen_constraints = connection.modifyScreenConstraints(screen_constraints);
+                        callback(error, screen_constraints);
+                        return;
+                    }
+                    throw error;
+                });
+            };
+
+            // by default, socket.io server is assumed to be deployed on your own URL
+            connection.socketURL = '/';
+
+            connection.socketMessageEvent = 'audio-video-screen-demo';
+
+            connection.session = {
+                audio: true,
+                video: true
+            };
+
+            connection.sdpConstraints.mandatory = {
+                OfferToReceiveAudio: true,
+                OfferToReceiveVideo: true
+            };
+
+            connection.videosContainer = document.getElementById('videos-container');
+            connection.onstream = function(event) {
+                if(document.getElementById(event.streamid)) {
+                    var existing = document.getElementById(event.streamid);
+                    existing.parentNode.removeChild(existing);
+                }
+                
+                var width = parseInt(connection.videosContainer.clientWidth / 2) - 20;
+                
+                if(event.stream.isScreen === true) {
+                    width = connection.videosContainer.clientWidth - 20;
+                }
+                
+                var mediaElement = getMediaElement(event.mediaElement, {
+                    title: event.userid,
+                    buttons: ['full-screen'],
+                    width: width,
+                    showOnMouseEnter: false
+                });
+                                
+                if(mediaElement.style.width != "1250px")
+                    connection.videosContainer.appendChild(mediaElement);
+
+                setTimeout(function() {
+                    mediaElement.media.play();
+                }, 5000);
+
+                mediaElement.id = event.streamid;
+            };
+
+            connection.onstreamended = function(event) {
+                var mediaElement = document.getElementById(event.streamid);
+                if(mediaElement) {
+                    mediaElement.parentNode.removeChild(mediaElement);
+                }
+            };
+
+            function disableInputButtons() {
+                document.getElementById('open-or-join-room').disabled = true;
+                document.getElementById('open-room').disabled = true;
+                document.getElementById('join-room').disabled = true;
+                document.getElementById('room-id').disabled = true;
+                document.getElementById('share-screen').disabled = false;
+            }
+
+            // ......................................................
+            // ......................Handling Room-ID................
+            // ......................................................
+
+            function showRoomURL(roomid) {
+                var roomHashURL = '#' + roomid;
+                var roomQueryStringURL = '?roomid=' + roomid;
+
+                var html = '<h2>Unique URL for your room:</h2><br>';
+
+                html += 'Hash URL: <a href="' + roomHashURL + '" target="_blank">' + roomHashURL + '</a>';
+                html += '<br>';
+                html += 'QueryString URL: <a href="' + roomQueryStringURL + '" target="_blank">' + roomQueryStringURL + '</a>';
+
+                var roomURLsDiv = document.getElementById('room-urls');
+                roomURLsDiv.innerHTML = html;
+
+                roomURLsDiv.style.display = 'block';
+            }
+
+            (function() {
+                var params = {},
+                    r = /([^&=]+)=?([^&]*)/g;
+
+                function d(s) {
+                    return decodeURIComponent(s.replace(/\+/g, ' '));
+                }
+                var match, search = window.location.search;
+                while (match = r.exec(search.substring(1)))
+                    params[d(match[1])] = d(match[2]);
+                window.params = params;
+            })();
+
+            var roomid = '';
+            if (localStorage.getItem(connection.socketMessageEvent)) {
+                roomid = localStorage.getItem(connection.socketMessageEvent);
+            } else {
+                roomid = connection.token();
+            }
+            document.getElementById('room-id').value = roomid;
+            document.getElementById('room-id').onkeyup = function() {
+                localStorage.setItem(connection.socketMessageEvent, this.value);
+            };
+
+            var hashString = location.hash.replace('#', '');
+            if(hashString.length && hashString.indexOf('comment-') == 0) {
+              hashString = '';
+            }
+
+            var roomid = params.roomid;
+            if(!roomid && hashString.length) {
+                roomid = hashString;
+            }
+
+            if(roomid && roomid.length) {
+                document.getElementById('room-id').value = roomid;
+                localStorage.setItem(connection.socketMessageEvent, roomid);
+
+                // auto-join-room
+                (function reCheckRoomPresence() {
+                    connection.checkPresence(roomid, function(isRoomExists) {
+                        if(isRoomExists) {
+                            connection.join(roomid);
+                            return;
+                        }
+
+                        setTimeout(reCheckRoomPresence, 5000);
+                    });
+                })();
+
+                disableInputButtons();
+            }
---
+++ demos/record/js/main.js
@@ -0,0 +1,133 @@
+
+
+function recordit(media_container)
+{
+
+	var mediaSource = new MediaSource();
+	mediaSource.addEventListener('sourceopen', handleSourceOpen, false);
+	var mediaRecorder;
+	var recordedBlobs;
+	var sourceBuffer;
+
+	//var gumVideo = document.querySelector('video#gum');
+
+	var gumVideo= media_container.find('video.gum')[0];	
+
+
+	var recordButton = media_container.find('button.record')[0];
+	var downloadButton = media_container.find('button.download')[0];
+	recordButton.onclick = toggleRecording;
+	downloadButton.onclick = download;
+
+	// window.isSecureContext could be used for Chrome
+	var isSecureOrigin = location.protocol === 'https:' ||
+	location.hostname === 'localhost';
+	//if (!isSecureOrigin) {
+	  //alert('getUserMedia() must be run from a secure origin: HTTPS or localhost.' +
+	    //'\n\nChanging protocol to HTTPS');
+	//  location.protocol = 'HTTPS';
+	//}
+
+	var constraints = {
+	  audio: true,
+	  video: true
+	};
+
+	function handleSuccess(stream) {
+	  recordButton.disabled = false;
+	  console.log('getUserMedia() got stream: ', stream);
+	  window.stream = stream;
+	  gumVideo.srcObject = stream;
+	}
+
+	function handleError(error) {
+	  console.log('navigator.getUserMedia error: ', error);
+	}
+
+	navigator.mediaDevices.getUserMedia(constraints).
+	    then(handleSuccess).catch(handleError);
+
+	function handleSourceOpen(event) {
+	  console.log('MediaSource opened');
+	  sourceBuffer = mediaSource.addSourceBuffer('video/webm; codecs="vp8"');
+	  console.log('Source buffer: ', sourceBuffer);
+	}
+
+
+	function handleDataAvailable(event) {
+	  if (event.data && event.data.size > 0) {
+	    recordedBlobs.push(event.data);
+	  }
+	}
+
+	function handleStop(event) {
+	  console.log('Recorder stopped: ', event);
+	}
+
+	function toggleRecording() {
+	  if (recordButton.textContent === 'Start Recording') {
+	    startRecording();
+	  } else {
+	    stopRecording();
+	    recordButton.textContent = 'Start Recording';
+	    downloadButton.disabled = false;
+	  }
+	}
+
+	function startRecording() {
+	  recordedBlobs = [];
+	  var options = {mimeType: 'video/webm;codecs=vp9'};
+	  if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+	    console.log(options.mimeType + ' is not Supported');
+	    options = {mimeType: 'video/webm;codecs=vp8'};
+	    if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+	      console.log(options.mimeType + ' is not Supported');
+	      options = {mimeType: 'video/webm'};
+	      if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+		console.log(options.mimeType + ' is not Supported');
+		options = {mimeType: ''};
+	      }
+	    }
+	  }
+	  try {
+	    mediaRecorder = new MediaRecorder(window.stream, options);
+	  } catch (e) {
+	    console.error('Exception while creating MediaRecorder: ' + e);
+	    alert('Exception while creating MediaRecorder: '
+	      + e + '. mimeType: ' + options.mimeType);
+	    return;
+	  }
+	  console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
+	  recordButton.textContent = 'Stop Recording';
+	  downloadButton.disabled = true;
+	  mediaRecorder.onstop = handleStop;
+	  mediaRecorder.ondataavailable = handleDataAvailable;
+	  mediaRecorder.start(10); // collect 10ms of data
+	  console.log('MediaRecorder started', mediaRecorder);
+	}
+
+	function stopRecording() {
+	  mediaRecorder.stop();
+	}
+
+
+
+	function download() {
+	  var blob = new Blob(recordedBlobs, {type: 'video/webm'});
+	  var url = window.URL.createObjectURL(blob);
+	  var a = document.createElement('a');
+	  a.style.display = 'none';
+	  a.href = url;
+	  a.download = 'test.webm';
+	  document.body.appendChild(a);
+	  a.click();
+	  setTimeout(function() {
+	    document.body.removeChild(a);
+	    window.URL.revokeObjectURL(url);
+	  }, 100);
+	}
+
+}
+
+//var el1 = $('.media_container').eq(0);
+//recordit(el1);
